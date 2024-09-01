7fa9045c8d66a560d42e45b37780f46af8c81821
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-01 18:20:12 +0100

    feat: review section

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Appointment from "@/components/UI/Home/appointment";
 import Hero from "@/components/UI/Home/hero";
 import Partners from "@/components/UI/Home/partners";
+import Reviews from "@/components/UI/Home/reviews";
 import Services from "@/components/UI/Home/services";
 
 const Page = () => {
@@ -10,6 +11,7 @@ const Page = () => {
       <Services />
       <Appointment />
       <Partners />
+      <Reviews />
     </>
   );
 };
