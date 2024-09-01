5f08ea7a0290f216b380747e0ac39056b29df32e
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-01 17:11:52 +0100

    feat: custom cursor component

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,4 @@
+import Cursor from "@/components/Common/Cursor";
 import Footer from "@/components/Layout/Footer";
 import Navbar from "@/components/Layout/Navbar";
 import PublicWrapper from "@/lib/providers/public-wrapper";
@@ -5,6 +6,7 @@ import PublicWrapper from "@/lib/providers/public-wrapper";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <PublicWrapper>
+      <Cursor />
       <Navbar />
       {children}
 
