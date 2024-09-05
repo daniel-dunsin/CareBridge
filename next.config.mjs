01a9e00cc7d0b49ad9e8d281af62ad14a0da6a58
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-06 00:54:00 +0100

    update: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,60 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "t4.ftcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.punchng.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "t3.ftcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "st3.depositphotos.com",
+      },
+      {
+        protocol: "https",
+        hostname: "us.123rf.com",
+      },
+      {
+        protocol: "https",
+        hostname: "victoria.mediaplanet.com",
+      },
+      {
+        protocol: "https",
+        hostname: "parade.com",
+      },
+      {
+        protocol: "https",
+        hostname: "shelleefisher.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
