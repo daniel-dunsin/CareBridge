a857e0d4b2a29617dea24d008bd99f650e5de3c6
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-06 01:31:05 +0100

    update: loader color

--- components/Common/Loaders/three-dots.tsx
+++ components/Common/Loaders/three-dots.tsx
@@ -3,9 +3,9 @@
 const ThreeDotsLoader = () => {
   return (
     <div className="flex items-center gap-1">
-      <div className="size-3 rounded-full bg-primaryBlue animate-bounce ease-out"></div>
-      <div className="size-3 rounded-full bg-primaryBlue animate-bounce ease-out [animation-delay:0.15s]"></div>
-      <div className="size-3 rounded-full bg-primaryBlue animate-bounce ease-out [animation-delay:0.3s]"></div>
+      <div className="size-3 rounded-full bg-primary animate-bounce ease-out"></div>
+      <div className="size-3 rounded-full bg-primary animate-bounce ease-out [animation-delay:0.15s]"></div>
+      <div className="size-3 rounded-full bg-primary animate-bounce ease-out [animation-delay:0.3s]"></div>
     </div>
   );
 };
--- components/UI/Home/hero.tsx
+++ components/UI/Home/hero.tsx
@@ -13,7 +13,7 @@ import useDimension from "@/lib/hooks/useDimension";
 const EarthModelView = dynamic(() => import("@/components/Common/Models/earth"), {
   ssr: false,
   loading: () => (
-    <div className="w-full h-full grid place-content-center">
+    <div className="w-full h-full border-8 flex items-center justify-center">
       <ThreeDotsLoader />
     </div>
   ),
