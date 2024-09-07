fa29a4513e9d0d36a4f69c35f8d3d69b623ef758
Adejare Daniel <adejaredaniel12@gmail.com>
2024-09-07 19:44:47 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   const allowedOrigins = [
     'http://localhost:3000',
     'https://carebridge-xi.vercel.app',
+    'https://bdmeds-frontend.onrender.com'
   ];
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: false, limit: '50mb' }));
