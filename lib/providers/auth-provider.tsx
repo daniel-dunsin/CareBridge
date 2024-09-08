e7a47cefb58dcb28b3aa3f7dd6d3fe0b2f6bd87f
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-08 23:29:42 +0100

    update: auth provider

--- lib/providers/auth-provider.tsx
+++ lib/providers/auth-provider.tsx
@@ -3,11 +3,20 @@
 import SessionCheckLoader from "@/components/Common/Loaders/session-check";
 import { useSession } from "next-auth/react";
 import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 type Props = React.PropsWithChildren;
 
 const AuthProvider: React.FC<Props> = ({ children }) => {
   const { data: session, status } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session && status === "unauthenticated") {
+      router.replace("/");
+    }
+  }, [session, status]);
 
   if (status === "loading") return <SessionCheckLoader />;
 
