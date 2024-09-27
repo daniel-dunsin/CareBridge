0ca96a2d74e9da4356ea5f492cb2a848b670b3a4
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-27 18:49:56 +0100

    fix: build error

--- components/UI/Meeting/MeetingCard.tsx
+++ components/UI/Meeting/MeetingCard.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
-import React from 'react';
-import { Button } from './ui/button';
-import { avatarImages } from '@/constants';
-import { cn } from '@/lib/utils';
-import { toast } from '@/components/ui/use-toast';
+import Image from "next/image";
+import React from "react";
+// import { avatarImages } from '@/constants';
+import { cn } from "@/lib/utils";
+import Button from "@/components/Common/Button";
+import { toastSuccess } from "@/lib/utils/toast";
 
 interface MeetingCardProps {
   title: string;
@@ -24,7 +24,7 @@ const MeetingCard = ({
   buttonIcon1,
   buttonText,
   handleClick,
-  link
+  link,
 }: MeetingCardProps) => {
   return (
     <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
@@ -40,7 +40,7 @@ const MeetingCard = ({
       </article>
       <article className="relative flex w-full">
         <div className="relative flex w-full max-sm:hidden">
-          {avatarImages.map((img, index) => (
+          {/* {avatarImages.map((img, index) => (
             <Image
               key={index}
               src={img}
@@ -52,7 +52,7 @@ const MeetingCard = ({
               })}
               style={{ top: 0, left: index * 28 }}
             />
-          ))}
+          ))} */}
           <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
             +5
           </div>
@@ -61,26 +61,17 @@ const MeetingCard = ({
         {!isPreviousMeeting && (
           <div className="flex gap-2">
             <Button onClick={handleClick} className="rounded bg-blue-1 px-6">
-              {buttonIcon1 && (
-                <Image src={buttonIcon1} alt="feature" width={20} height={20} />
-              )}
+              {buttonIcon1 && <Image src={buttonIcon1} alt="feature" width={20} height={20} />}
               &nbsp; {buttonText}
             </Button>
             <Button
               onClick={() => {
                 navigator.clipboard.writeText(link);
-                toast({
-                  title: 'Link Copied'
-                });
+                toastSuccess("Link Copied");
               }}
               className="bg-dark-4 px-6"
             >
-              <Image
-                src="/icons/copy.svg"
-                alt="feature"
-                width={20}
-                height={20}
-              />
+              <Image src="/icons/copy.svg" alt="feature" width={20} height={20} />
               &nbsp; Copy Link
             </Button>
           </div>
